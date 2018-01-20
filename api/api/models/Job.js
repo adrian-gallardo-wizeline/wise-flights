c10bddc8afdfc322d99e17a044b01f7fa7ae32a6
josebreijo <30603119+josebreijo@users.noreply.github.com>
2018-01-19 22:08:28 -0600

    Add job model

---
+++ api/api/models/Job.js
@@ -0,0 +1,23 @@
+/**
+ * Job.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+  attributes: {
+
+    startDate: { type: 'string' },
+    endDate: { type: 'string' },
+    completed: { type: 'boolean', defaulsTo: false },
+    attempts: { type: 'number', defaultsTo: 0 },
+
+    url: { type: 'string', allowNull: true },
+    price: { type: 'number', allowNull: true },
+    provider: { type: 'string', allowNull: true },
+
+    owner: { model: 'query' },
+  }
+};
+
