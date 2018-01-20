79f086bb951a4c58f3103c9bb31836e41c2eabb1
josebreijo <30603119+josebreijo@users.noreply.github.com>
2018-01-20 00:30:31 -0600

    Add intervals generation method

--- api/api/controllers/QueryController.js
+++ api/api/controllers/QueryController.js
@@ -1,8 +1,42 @@
-/**
- * QueryController
- *
- * @description :: Server-side logic for managing queries
- * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
- */
+const moment = require('moment');
 
-module.exports = {};
+const DATE_FORMAT = 'YYYY-MM-DD';
+
+function* generateIntervals(payload) {
+  const { fromDate, toDate, minDays, maxDays } = payload;
+
+  const startDate = moment(fromDate);
+  const endDate = moment(toDate);
+
+  for (let diff = minDays; diff <= maxDays; diff++) {
+    const left = startDate.clone();
+
+    while (left.isSameOrBefore(endDate)) {
+
+      const right = left.clone();
+      right.add(diff, 'days');
+
+      yield {
+        startDate: left.format(DATE_FORMAT),
+        endDate: right.format(DATE_FORMAT)
+      };
+
+      left.add(1, 'days');
+    }
+  }
+}
+
+
+const actions = {
+
+  create(req, res) {
+    const payload = req.body;
+    const intervals = generateIntervals(payload);
+
+    for (let interval of intervals) {
+    }
+  }
+
+};
+
+module.exports = actions;
