8e5523944ef14b7bea32b8382aa6c26485882e16
Adrian Gallardo <adrian.gallardo@wizeline.com>
2018-01-20 14:28:52 -0600

    Listen to new jobs

--- crawl/index.js
+++ crawl/index.js
@@ -2,25 +2,39 @@ const express = require('express');
 const axios = require('axios');
 const app = express();
 
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 require('dotenv').config();
 
 const FetcherApp = require('./fetcher/FetcherApp');
 
-app.get('/awake', function (req, res) {
-  res.send('Hello World!');
+const fetcherApp = new FetcherApp();
+const JOBS = [];
+
+
+app.get('/newJobs', function (req, res) {
+  const jobs = req.body || [];
+  console.log('-- INCOMING NEW JOBS --', jobs.length);  
+  res.sendStatus(200);
+  processJobs(jobs);
 });
 
 app.listen(process.env.PORT, function () {
+  console.log(`Listening on port ${process.env.PORT}!`);
   init();
-  console.log('Example app listening on port 3000!');
 });
 
 async function init() {
   const jobs = await getJobs();
-  const fetcherApp = new FetcherApp();
+  processJobs(jobs);
+}
 
-  await fetcherApp.processJobs(jobs);
-
+async function processJobs(newJobs){
+  JOBS.push(...newJobs);
+  await fetcherApp.processJobs(JOBS);
   console.log('done', fetcherApp.getResults());
 }
 
